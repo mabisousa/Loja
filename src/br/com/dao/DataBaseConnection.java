f21b325a56a5a65e542c6cbd5c3e1241b425e6bf
Mabi Sousa <maria_gs_cruz@estudante.sc.senai.br>
2021-05-11 16:50:31 -0300

    Configuração de conexão com o banco de dados

---
+++ src/br/com/dao/DataBaseConnection.java
@@ -0,0 +1,32 @@
+package br.com.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DataBaseConnection {
+	private static DataBaseConnection dataBaseConnection;
+	private Connection connection;
+	
+	private DataBaseConnection() {
+		try {
+			Class.forName("com.mysql.jdbc.Driver").newInstance();
+			connection = DriverManager.getConnection(
+				"jdbc:mysql://127.0.0.1:3306/" + 
+				"loja?user=root&useSSL=false"
+				);
+		} catch (Exception e) {
+			System.out.println("/nErro!/nNão foi possível conectar no banco de dados. Contate o suporte.");
+		}
+	}
+	
+	public static DataBaseConnection getInstance() {
+		
+		if(dataBaseConnection == null) dataBaseConnection = new DataBaseConnection();
+		
+		return dataBaseConnection;
+	}
+	
+	public Connection getConnection() {
+		return connection;
+	}
+}

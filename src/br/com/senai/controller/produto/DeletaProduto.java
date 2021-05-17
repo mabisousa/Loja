94a097a03608692992925fa9a15d1a72b35d5968
Mabi Sousa <maria_gs_cruz@estudante.sc.senai.br>
2021-05-17 11:42:51 -0300

    Refatoração para deletar dados no BD

--- src/br/com/senai/controller/produto/DeletaProduto.java
+++ src/br/com/senai/controller/produto/DeletaProduto.java
@@ -1,30 +1,65 @@
 package br.com.senai.controller.produto;
 
-import java.util.List;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.util.Scanner;
 
-import br.com.senai.model.ProdutoModel;
+import br.com.dao.DataBaseConnection;
 
 public class DeletaProduto {
-	ListaProduto listaProduto = new ListaProduto();
-	Scanner tec = new Scanner(System.in);
-	public void removerProdutos(List<ProdutoModel> produtos) {
+	
+	private ListaProduto listaProduto = new ListaProduto();
+	private Scanner tec = new Scanner(System.in);
+	private Connection connection;
+	
+	public DeletaProduto(){
+		connection = DataBaseConnection.getInstance().getConnection();
+	}
+	
+	public boolean verificaSeExisteProduto(int idDoProduto) {
+		PreparedStatement preparedStatement;
+		try {
+			String sql = "SELECT * FROM produto WHERE codigoDoProduto = ?";
+			preparedStatement = connection.prepareStatement(sql);
+			preparedStatement.setInt(1, idDoProduto);
+			ResultSet resultSet = preparedStatement.executeQuery();
+			if (!resultSet.next()) {
+				System.out.println("Esse produto não existe");
+				return false;
+			} else {
+				return true;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			return true;
+		}
+		
+	}
+	
+	public void removerProdutos() {
+		PreparedStatement preparedStatement;
 		System.out.println("--- REMOVER PRODUTO ---");
-		if (produtos.size() <= 0) {
-			System.out.println("Não há produtos cadastrados");
+		if(listaProduto.listarProdutos() == null) {
 			return;
 		}
-
-		listaProduto.listarProdutos();
-
 
 		System.out.println("Informe o ID do produto  ser removido");
-		int id = tec.nextInt();
+		int idDoProduto = tec.nextInt();
 
-		if (id >= produtos.size()) {
-			System.out.println("Produto não cadastrado");
+		try {
+			
+			if(!verificaSeExisteProduto(idDoProduto)) {
+				return;
+			}
+			String sql = "DELETE FROM produto WHERE codigoDoProduto = ?";
+			preparedStatement = connection.prepareStatement(sql);
+			preparedStatement.setInt(1, idDoProduto);
+			preparedStatement.execute();
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.out.println("Não foi possível excluir essa informação.");
 			return;
 		}
-		produtos.remove(id - 1);
 	}
 }

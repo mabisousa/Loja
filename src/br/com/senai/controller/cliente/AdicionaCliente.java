92524e131879acfe5c98383f6a0363b1ee9c9963
Mabi Sousa <maria_gs_cruz@estudante.sc.senai.br>
2021-05-20 11:43:47 -0300

    Criação de validação de clientes duplicados

--- src/br/com/senai/controller/cliente/AdicionaCliente.java
+++ src/br/com/senai/controller/cliente/AdicionaCliente.java
@@ -1,8 +1,8 @@
 package br.com.senai.controller.cliente;
 
-
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.util.Scanner;
 
 import br.com.dao.DataBaseConnection;
@@ -12,28 +12,50 @@ public class AdicionaCliente {
 
 	Scanner tec = new Scanner(System.in);
 	private Connection connection;
+	ClienteModel clienteModel = new ClienteModel();
 
 	public AdicionaCliente() {
 		connection = DataBaseConnection.getInstance().getConnection();
 	}
 
-	public ClienteModel definirCliente() {
-		ClienteModel clienteModel = new ClienteModel();
-		PreparedStatement preparedStatement;
+	public ResultSet definirCliente() {
+
 		System.out.print("Informe o nome do cliente: ");
 		clienteModel.setNomeDoCliente(tec.next());
+		//String nome = clienteModel.getNomeDoCliente();
 
 		try {
-			String sql = "INSERT INTO cliente VALUES ?";
-			preparedStatement = connection.prepareStatement(sql);
-
+			String sql = "INSERT INTO cliente (nome) VALUES (?)";
+			PreparedStatement preparedStatement = connection.prepareStatement(sql);
 			preparedStatement.setString(1, clienteModel.getNomeDoCliente());
 
 			preparedStatement.execute();
-		} catch (Exception e) {
-			System.out.println("Erro ao cadastrar os dados.");
-		}
+			} catch (Exception e) {
+				System.out.println("Erro ao cadastrar os dados.");
+			}
+		
+//		try {
+//			String sql = "select * from cliente";
+//
+//			PreparedStatement preparedStatement = connection.prepareStatement(sql);
+//			ResultSet resultSet = preparedStatement.executeQuery();
+//			
+//			while (resultSet.next()) {
+//				if (!resultSet.getString("nome").equals(nome)) {
+//					cadastrarCliente(nome);
+//				} else {
+//					return null;
+//				}
+//			}
+//		} catch (Exception e) {
+//			System.out.println("Erro ao cadastrar os dados.");
+//		}
+		return null;
 
-		return clienteModel;
 	}
+
+	public void cadastrarCliente(String nome) {
+		
+	}
+	
 }
